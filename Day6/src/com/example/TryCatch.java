afe3b7a15b4ea1e74aa720a9dce46d6e81065b15
Pravinkumar Raja <Pravinkumar Raja/pravinkumarrit@gmail.com>
2025-11-07 10:48:50 +0530

    Try-catch updated

--- Day6/src/com/example/TryCatch.java
+++ Day6/src/com/example/TryCatch.java
@@ -5,6 +5,28 @@ import java.util.Scanner;
 public class TryCatch {
 	public static int arr[]=new int[5];
 	
+	
+	public static void div(int x,int y)
+	{
+		try {
+			System.out.println("Welcome to Division Program");
+			System.out.println("Division of "+x+" / "+y+" is "+(x/y));
+			
+		}
+		catch(Exception e)
+		{
+			
+			e.printStackTrace();
+		}
+		try {
+			arr[11]=77;
+		}
+		catch (Exception e){
+			System.err.println(e);
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		arr[0]=22;
 		arr[1]=33;
@@ -14,16 +36,8 @@ public class TryCatch {
 		int x=s.nextInt();
 		System.out.println("Enter y value");
 		int y=s.nextInt();
-		try {
-			System.out.println("Welcome to Division Program");
-			System.out.println("Division of "+x+" / "+y+" is "+(x/y));
-			arr[11]=77;
-		}
-		catch(Exception e)
-		{
-			
-			System.err.println(e.getMessage());
-		}
+		
+		TryCatch.div(x,y);
 		System.out.println("Bye.......");
 	}
 }
