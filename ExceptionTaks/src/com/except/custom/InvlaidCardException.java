6d3c033359971cbab06917ade5ad3ed94d082342
Pravinkumar Raja <Pravinkumar Raja/pravinkumarrit@gmail.com>
2025-11-29 16:27:02 +0530

    Tasks Added

---
+++ ExceptionTaks/src/com/except/custom/InvlaidCardException.java
@@ -0,0 +1,10 @@
+package com.except.custom;
+
+public class InvlaidCardException extends Exception{
+	
+	public void InvalidCardException()
+	{
+		this.InvalidCardException();
+	}
+
+}
---
+++ ExceptionTaks/src/com/except/custom/OnlinePaymentSystem.java
@@ -0,0 +1,19 @@
+package com.except.custom;
+
+import java.util.Scanner;
+
+public class OnlinePaymentSystem {
+	public static int cardNo;
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the card no for payment");
+		cardNo=s.nextInt();
+		try {
+			if()
+			{
+				
+			}
+		}
+	}
+
+}
